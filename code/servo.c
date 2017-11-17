dc6e7203175e9c1a9522705aeaaf631fde2eff25
Aidan Collins <ac5152@nyu.edu>
2017-11-17 15:12:49 -0500

    motor calculations

--- code/servo.c
+++ code/servo.c
@@ -1,3 +1,6 @@
-// we are spinning it clockwise
-// we want 100% DC to be full power, which is at 2000us
-// this meas the freq of the PWM is 500
+// 16-bit timer witha 265 prescaller set to fast pwm with ICR1 as top
+// ICR1 = 1250 
+// period = 20ms
+//full reverse 1ms | slow reverse 1.45ms | 	off 1.5ms| 	 low forward 1.55ms | 	 full ahead 2ms |
+// 	OCR1X 63 	   | 	OCR1X = 91       | OCR1X = 93|  	OCR1X = 97 		|  		OCR1X = 125 |
+//
