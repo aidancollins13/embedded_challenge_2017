8300280fd825013a9a3d883bb991927cd6fc3bdf
Aidan Collins <ac5152@nyu.edu>
2017-12-05 18:17:10 -0500

    added receive_final

--- code/receive_final/receive_final.ino
+++ code/receive_final/receive_final.ino
@@ -2,6 +2,7 @@
 #include"SPI.h"
 #include"RF24.h"
 
+long readings[2];
 int radio_number = 1;
 void setup(){
 	servo_setup();
@@ -11,6 +12,11 @@ void setup(){
 
 void loop(){
 
+	get_reading(&readings);
+	set_motor(0,readings[0]);
+	set_motor(0,readings[1]);
+	delay(100);
+	
 }
 
 
@@ -33,4 +39,29 @@ void wireless_setup(int radio_number){
 
 }
 
+void get_readings(&readings){
 
+	while(radio.available()){
+		radio.read(&readings, sizeof(long) *2);
+ 	 }
+}
+
+// This sets up timer 1 to be fast pwm with a 256 prescaler
+// and a 1250 top, pins 9 and 10 are outputs.
+void servo_setup(){
+
+	TCCR1A = 0b10100010;
+	TCCR1B = 0b00011100;
+	ICR1 = 1250;
+	DDRB = 0x06;
+}
+
+// this function takes in a reading value
+// 	assumed to be a 10 bit value(like an analog sensor would read)
+// 	and a motor, motor 0 changes oin 9, motor 1 change pin 10
+void set_motor(int motor, int speed){
+	if (motor == 0)
+		OCR1A = map(speed, 3, 40, 63,125);
+	if (motor == 1)
+		OCR1B = map(speed, 3, 40, 63, 125);
+}
