a040ca27fc771000feb9915c57265171a932900a
Ray Mohabir <rm3887@nyu.edu>
2017-12-05 20:05:22 -0500

    a

--- code/car_arduino/car_arduino.ino
+++ code/car_arduino/car_arduino.ino
@@ -69,11 +69,11 @@ long read_dist(int sensor){
     echoPin = 6;
   }
   long duration, distance;
-  digitalWrite(trigPin, LOW);  
+  PORTD  &= !(1 << trigPin); //  digitalWrite(trigPin, LOW);  
   delayMicroseconds(2); 
-  digitalWrite(trigPin, HIGH);
+  PORTD |= (1<<trigPin); //digitalWrite(trigPin, HIGH);
   delayMicroseconds(10); 
-  digitalWrite(trigPin, LOW);
+  PORTD &= !(1 << trigPin);//  digitalWrite(trigPin, LOW);  
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
 
--- code/transmit_final/transmit_final.ino
+++ code/transmit_final/transmit_final.ino
@@ -3,13 +3,14 @@
 
 
 RF24 radio(7,8);
-byte addresses[][5] = {"15_T", "15_R"};
+byte addresses[][6] = {"15_TR", "15_RR"};
 int radio_number = 0;
 
 long s1,s0;
 long readings[2];
 
 void setup(){
+  Serial.begin(9600); 
 	wireless_setup(radio_number);
 	sensor_setup();
 }
@@ -19,7 +20,6 @@ void loop(){
 	s1 = read_dist(1);
 	
 	send_readings(s0,s1);
-
 	delay(100);
 
 }
@@ -45,6 +45,8 @@ long read_dist(int sensor){
 	distance = (duration/2) / 29.1;
 
 	Serial.print("SENSOR:\t");
+  Serial.print(sensor);
+  Serial.print(" - ");
 	Serial.print(distance);
 	Serial.println(" cm");
 	return distance;
@@ -52,15 +54,19 @@ long read_dist(int sensor){
 
 
 void sensor_setup(){
-	DDRD |=   0b00100100; // set pin 2 and 5 as output
-	DDRD &= !(0b01001000);//set pin 3 and 6 as input
-	PORTD |= 0b01001000; //enable pullup on 3 and 6, the inputs
-
+	//DDRD |=   0b00100100; // set pin 2 and 5 as output
+	//DDRD &= !(0b01001000);//set pin 3 and 6 as input
+  //DDRD = 0b10100100;
+	//PORTD |= 0b01001000; //enable pullup on 3 and 6, the inputs
+ pinMode(2, OUTPUT);
+  pinMode(3, INPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6,INPUT);
 }
 
 void wireless_setup(int radio_number){
 	radio.begin();
-	radio.setPALevel(RF24_PA_HIGH);
+	radio.setPALevel(RF24_PA_LOW);
 
 	if(radio_number){ 	//receive
 		radio.openWritingPipe(addresses[1]);
@@ -69,7 +75,7 @@ void wireless_setup(int radio_number){
 
 	else{ 				//transmit
 		radio.openWritingPipe(addresses[0]);
-		radio.openReadingPipe(1,addresses[0]);
+		radio.openReadingPipe(1,addresses[1]);
 
 	}
 
@@ -79,11 +85,29 @@ void wireless_setup(int radio_number){
 
 void send_readings(long s0, long s1){
 	readings[0] = s0;
-	readings[1] = s1;
+	readings[1] = 1;
+  Serial.println("A");
 	radio.stopListening();
-	if(!radio.write(readings, sizeof(long) *2)){
+  Serial.println("B");
+  if (!radio.available())
+    Serial.println("Timed");
+  Serial.println("C");
+  unsigned long start_time = 2;
+	if(!radio.write(&start_time, sizeof(unsigned long))){
 		Serial.println("WIRELESS:\t failed");
  	}
-	radio.startListening();
+  radio.startListening();
+    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
+    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
+    
+    while ( ! radio.available() ){                             // While nothing is received
+      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
+          timeout = true;
+          break;
+      }      
+    }
+        
+    if ( timeout )                                             // Describe the results
+        Serial.println(F("Failed, response timed out."));
 
 }
